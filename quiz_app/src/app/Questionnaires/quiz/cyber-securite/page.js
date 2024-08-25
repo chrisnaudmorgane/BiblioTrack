3349a5cd4fd46b9f4a08b323f016b7b088c7f9ce
chrisnaudmorgane <chrisnaud.agossou@epitech.eu>
2024-08-25 15:24:47 +0100

    rectification

--- quiz_app/src/app/Questionnaires/quiz/cyber-securite/page.js
+++ quiz_app/src/app/Questionnaires/quiz/cyber-securite/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import cyberSecurityQuiz from '../../question_crypto/cyber_sec';
 
--- quiz_app/src/app/Questionnaires/quiz/developpement-web/page.js
+++ quiz_app/src/app/Questionnaires/quiz/developpement-web/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import webDevQuiz from '../../question_crypto/développement_web';
 
