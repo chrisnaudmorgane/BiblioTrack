8b7552d034c37006020e9dc0b88daa00c90ba86f
chrisnaudmorgane <chrisnaud.agossou@epitech.eu>
2024-09-03 19:32:16 +0100

    add another quiz for the commerce

---
+++ quiz_app/src/app/Questionnaires/question_crypto/commerce.js
@@ -0,0 +1,43 @@
+const commerce = [
+    {
+        question: "Qu'est-ce-que le e-commerce ?",
+        options: [
+            "A) C'est de faire des achats en ligne",
+            "B) Faire de la vente en ligne",
+            "C) C'est l'achat et la vente de produits en ligne",
+            "D) Regarder des vidéos en ligne"
+        ],
+        correctAnswer: "C",
+    },
+    {
+        question: "Quel est l'un des avantages du e-commerce ?",
+        options: [
+            "A) Acheter à tout moment, 24h/24",
+            "B) Ne jamais recevoir ce qu'on achète",
+            "C) Marcher jusqu'au magasin",
+            "D) Ne jamais dépenser d'argent",
+        ],
+        correctAnswer: "A",
+    },
+    {
+        question: "Quels sont les principaux réseaux sociaux utilisés pour l'affiliation de l'e-commerce ?",
+        options: [
+            "A) Facebook",
+            "B) Whatsapp",
+            "C) Tiktok",
+            "D) Instagram",
+            "E) Tous les réponses sont possibles",
+        ],
+        correctAnswer: "E"
+    },
+    {
+        question: "Whatsapp Business est-il un moyen de vendre ces produits ?",
+        options: [
+            "A) Oui",
+            "B) Non",
+        ],
+        correctAnswer: "A",
+    }
+];
+
+export default commerce;
