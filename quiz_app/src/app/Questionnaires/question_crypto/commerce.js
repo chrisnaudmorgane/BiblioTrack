eec22316d644a835678854db97fc5a25fa2106da
chrisnaudmorgane <chrisnaud.agossou@epitech.eu>
2024-09-05 16:33:30 +0100

    add another question for the commerce

--- quiz_app/src/app/Questionnaires/question_crypto/commerce.js
+++ quiz_app/src/app/Questionnaires/question_crypto/commerce.js
@@ -37,7 +37,108 @@ const commerce = [
             "B) Non",
         ],
         correctAnswer: "A",
-    }
+    },
+    {
+        question: "Que signifie le e dans le e-commerce ?",
+        options: [
+            "A) Le e signifie le commerce en ligne",
+            "B) Le e signifie le commerce physique",
+        ],
+        correctAnswer: "A",
+    },
+    {
+        question: "A quoi sert Shopify ?",
+        options: [
+            "A) A gérer et créer une boutique en ligne",
+            "B) A gérer un restaurant",
+            "C) A créer des vidéos en ligne",
+            "D) A développer des applications mobiles",
+        ],
+        correctAnswer: "A",
+    },
+    {
+        question: "Shopify est-il payant ?",
+        options: [
+            "A) Oui",
+            "B) Non",
+        ],
+        correctAnswer: "A",
+    },
+    {
+        question: "Quelle est la principale fonction d'une plateforme de e-commerce ?",
+        options: [
+            "A) Envoyer des emails",
+            "B) Héberger des sites web",
+            "C) Vendres des produits et services en ligne",
+            "D) Créer des publicités",
+        ],
+        correctAnswer: "C",
+    },
+    {
+        question: "Devenons-nous livrer les produits nous mêmês ?",
+        options: [
+            "A) Oui",
+            "B) Non",
+        ],
+        correctAnswer: "B",
+    },
+    {
+        question: "Quel mode de paiement est couramment utilisé dans le e-commerce ?",
+        options: [
+            "A) Chèque bancaire",
+            "B) Carte bancaire",
+            "C) Espèces",
+            "D) Troc",
+        ],
+        correctAnswer: "B",
+    },
+    {
+        question: "Que signifie le terme \"dropshipping\" dans le e-commerce ?",
+        options: [
+            "A) Vendre des produits directement depuis le fabricant au client sans stocker d'inventaire",
+            "B) Livrer des produits via des drones",
+            "C) Utiliser des coupons pour acheter en ligne",,
+            "D) Vendre uniquement des produits numériques"
+        ],
+        correctAnswer: "A",
+    },
+    {
+        question: "Quel est l'avantage principal du e-commerce par rapport au commerce traditionnel ?",
+        options: [
+            "A) Avoir un magasin physique",
+            "B) Vendre 24/7, sans contraintes géographique",
+            "C) Offrir un service client en personne",
+            "D) Limiter l'audience aux locaux",
+        ],
+        correctAnswer: "B",
+    },
+    {
+        question: "ChatGpt facilite votre vie",
+        options:  [
+            "A) Faux",
+            "B) Vrai",
+        ],
+        correctAnswer: "B",
+    },
+    {
+        question: "Qu'est-ce que le \"SEO\" dans le contexte du e-commerce ?",
+        options:  [
+            "A) Un service de livraison express",
+            "B) L'optimisation des moteurs de recherche pour mieux référencer une boutique en ligne",
+            "C) Un type de promotion par email",
+            "D) Un service client en ligne",
+        ],
+        correctAnswer: "B",
+    },
+    {
+        question: "Où peut-on trouver des fournisseurs ?",
+        options: [
+            "A) Aliexpress",
+            "B) Chrome",
+            "C) Shopify"
+        ],
+        correctAnswer: "A",
+    },
 ];
 
 export default commerce;
